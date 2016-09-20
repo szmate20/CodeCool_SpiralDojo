bba2f57bb8f7017269726f54a4557175bbc810fc
Szabó Máté <larde15@gmail.com>
2016-09-20 10:12:31 +0200

    last number is correct

--- src/spiralDojo/Spiral.java
+++ src/spiralDojo/Spiral.java
@@ -5,20 +5,30 @@ import java.util.Arrays;
 public class Spiral {
 	Integer[][] array;
 	
-	public void createSpiral(int N) {
+	public Integer[][] createSpiral(int N) {
 		array = new Integer[N][N];
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array[i].length; j++) {
-				if (i == 0 && j < N) {
-					array[i][j] = j + 1;
+		if (!(N  < 1)) {
+			int counter = 0;
+			for (int i = 0; i < array.length; i++) {
+				for (int j = 0; j < array[i].length; j++) {
+					if (i == 0 && j < N) {
+						counter++;
+						array[i][j] = counter;
+					}
+					else if (i == N-1 && j < N)  {
+						counter++;
+						array[i][N-1] = counter - 1;
+					}
+					
 				}
 			}
 		}
+		return array;
 	}
 	
 	public static void main(String[] args) {
 		Spiral spiral = new Spiral();
-		spiral.createSpiral(5);
+		spiral.createSpiral(3);
 		
 		for (int i = 0; i < spiral.array.length; i++) {
 			System.out.println(Arrays.toString(spiral.array[i]));
